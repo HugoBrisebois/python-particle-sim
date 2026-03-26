d273bcb974acaa9810a9977077652abadb9c4690
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-03-26 18:06:44 -0400

    Refactor main.py to enhance circle drawing functionality and update rendering logic

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from cmath import rect
 from math import radians
-from turtle import color, width
-
+import random
+from turtle import circle, color, width
+import random
 import pygame
 
 # Initialize the pygame window
 pygame.init()
 surface = pygame.display.set_mode((1080, 640))
 clock = pygame.time.Clock()
-color = (255, 192, 203)
 running = True
+
+# Color vars
+color = (255, 192, 203)
+red = (255, 0, 0)
+blue = (0, 0, 128)
+green = (0, 128, 0)
+
+
+
+def Draw_Circle():
+    
+    # vars for the circle function
+    radius = 5
+    pos = []
+    circle_pos = (500, 500)
+    
+    
+    pygame.draw.circle(surface, color, circle_pos, radius, width=0)
+    print("circle drawn")
+
 
 while running:
     # poll for events
@@ -21,16 +41,18 @@ while running:
     pygame.display.get_surface()
     
     # fill the screen with a color to wipe away anything from the last frame
-    surface.fill("purple")
+    surface.fill("black")
     
     # render the physics engine
-    pygame.draw.rect(surface, color, pygame.Rect(30,30,60,60))
+    
+    # draw a circle
+    Draw_Circle()
     
     
     
     # flip() the display to put the physics engine on screen
     pygame.display.flip()
-    pygame.time.wait(3000) # wait for 3 seconds
+    
         
     
     
